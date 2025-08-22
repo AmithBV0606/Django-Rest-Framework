983393da084ea754fdf7e9d9f7d01d5324f69892
Amith B V <amithrao0606@gmail.com>
2025-08-22 08:13:21 +0530

    Modal serialization (Function-Based View Get Method).

--- api/views.py
+++ api/views.py
@@ -1,6 +1,12 @@
 from django.http import JsonResponse
 from students.models import Student
 
+from .serializers import StudentSerializer
+
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.decorators import api_view
+
 # Static Data JSON response :
 # def studentsView(request):
 #     # student = {
@@ -16,7 +22,16 @@ from students.models import Student
 #     return JsonResponse(students, safe=False)
 
 # Manual Serialization : Converting the Query Set into the list
+# def studentsView(request):
+#     students = Student.objects.all()
+#     students_list = list(students.values())
+#     return JsonResponse(students_list, safe=False)
+
+# NOTE : If you want your view to be accessed by only specific request type, we can use "api_view"
+
+@api_view(['GET'])
 def studentsView(request):
-    students = Student.objects.all()
-    students_list = list(students.values())
-    return JsonResponse(students_list, safe=False)
+    if request.method == "GET":
+        students = Student.objects.all() # Query Set
+        serializer = StudentSerializer(students, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
