f27e1ae85e81342d6ce192e540e584b1ee9670dc
Amith B V <amithrao0606@gmail.com>
2025-08-22 08:08:56 +0530

    Manual Serialization.

--- api/views.py
+++ api/views.py
@@ -1,13 +1,22 @@
 from django.http import JsonResponse
 from students.models import Student
 
-# Create your views here.
+# Static Data JSON response :
+# def studentsView(request):
+#     # student = {
+#     #     'id': 1,
+#     #     'name': 'Amith',
+#     #     'class': 'CS'
+#     # }
+#     return JsonResponse(student)
+
+# Dynamic data coming from DB as JSON response : This won't work
+# def studentsView(request):
+#     students = Student.objects.all()
+#     return JsonResponse(students, safe=False)
+
+# Manual Serialization : Converting the Query Set into the list
 def studentsView(request):
-    # student = {
-    #     'id': 1,
-    #     'name': 'Amith',
-    #     'class': 'CS'
-    # }
     students = Student.objects.all()
     students_list = list(students.values())
     return JsonResponse(students_list, safe=False)
