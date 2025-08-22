b8ed79f8dc8eb384a82a38fa732dba15876f66de
Amith B V <amithrao0606@gmail.com>
2025-08-22 19:25:28 +0530

    Storing Data Using Serializers using POST request.

--- api/views.py
+++ api/views.py
@@ -29,9 +29,16 @@ from rest_framework.decorators import api_view
 
 # NOTE : If you want your view to be accessed by only specific request type, we can use "api_view"
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def studentsView(request):
     if request.method == "GET":
         students = Student.objects.all() # Query Set
         serializer = StudentSerializer(students, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
+    elif request.method == "POST":
+        serializer = StudentSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        print(serializer.errors)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
