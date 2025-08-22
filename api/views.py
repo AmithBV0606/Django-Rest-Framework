31776b94db14eef9b8a31d5bd68d3df50da214a7
Amith B V <amithrao0606@gmail.com>
2025-08-22 20:12:39 +0530

    Update Operation On Student.

--- api/views.py
+++ api/views.py
@@ -31,14 +31,33 @@ from rest_framework.decorators import api_view
 
 @api_view(['GET', 'POST'])
 def studentsView(request):
+    # Fetch data
     if request.method == "GET":
         students = Student.objects.all() # Query Set
         serializer = StudentSerializer(students, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
-    elif request.method == "POST":
+    elif request.method == "POST": # Store data
         serializer = StudentSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         print(serializer.errors)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET', 'PUT'])
+def studentDetailView(request, pk):
+    try:
+        student = Student.objects.get(pk=pk)
+    except Student.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == "GET":
+        serializer = StudentSerializer(student)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    elif request.method == "PUT":
+        serializer = StudentSerializer(student, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
