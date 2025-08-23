8f22930242a835ebffe25390dd22345eb3acc7c2
Amith B V <amithrao0606@gmail.com>
2025-08-23 16:23:08 +0530

    Class-Based update employee.

--- api/views.py
+++ api/views.py
@@ -103,3 +103,13 @@ class EmployeeDetails(APIView):
         employee = self.get_object(pk)
         serializer = EmployeeSerializer(employee)
         return Response(serializer.data, status=status.HTTP_200_OK)
+
+    # Update the individual employee :
+    def put(self, request, pk):
+        employee = self.get_object(pk)
+        serializer = EmployeeSerializer(employee, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
