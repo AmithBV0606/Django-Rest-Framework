5eb0ad4946d3003adc5ae4331b778cedc7c19fd0
Amith B V <amithrao0606@gmail.com>
2025-08-25 11:37:43 +0530

    Typo fix!!

--- blogs/serializers.py
+++ blogs/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 from .models import Blog, Comment
 
-class CommentSerializer(serializers.ModelField):
+class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Comment
         fields = "__all__"
