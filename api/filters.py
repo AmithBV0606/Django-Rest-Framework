cfb21c8c9f7b576c1f45c994efb30ba03d7341ce
Amith B V <amithrao0606@gmail.com>
2025-08-26 08:34:58 +0530

    Custom Filter Employees By Name And ID.

--- api/filters.py
+++ api/filters.py
@@ -5,7 +5,10 @@ from employees.models import Employee
 
 class EmployeeFilter(django_filters.FilterSet):
     designation = django_filters.CharFilter(field_name='designation', lookup_expr='iexact')
+    emp_name = django_filters.CharFilter(field_name='emp_name', lookup_expr='icontains')
+    id = django_filters.RangeFilter(field_name='id')
+    # emp_id = django_filters.RangeFilter(field_name='emp_id') # - This won't work because the range filter only works on Integer fields
 
     class Meta:
         model = Employee
-        fields = ['designation']
+        fields = ['designation', 'emp_name', 'id']
